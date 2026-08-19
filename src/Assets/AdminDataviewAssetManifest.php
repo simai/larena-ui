711a99a03074f0cbbcbea70246af194be1b8a616
Rim Zabarov <rim@zabarov.com>
2026-08-19 20:42:51 +0300

    fix: contain dataview within cms workspace

--- src/Assets/AdminDataviewAssetManifest.php
+++ src/Assets/AdminDataviewAssetManifest.php
@@ -7,7 +7,7 @@ namespace Larena\Ui\Assets;
 final class AdminDataviewAssetManifest
 {
     public const ASSET_KEY = 'ui.admin.dataview.css';
-    public const ASSET_REVISION = '20260819-7';
+    public const ASSET_REVISION = '20260819-8';
 
     /** @return array<string, mixed> */
     public static function publicationAsset(): array
