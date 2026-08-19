d6a89845643cda385586b5d12fd4a5c33c09934a
Rim Zabarov <rim@zabarov.com>
2026-08-19 17:05:04 +0300

    fix(ui): contain smart table on mobile

--- src/Assets/AdminDataviewAssetManifest.php
+++ src/Assets/AdminDataviewAssetManifest.php
@@ -7,7 +7,7 @@ namespace Larena\Ui\Assets;
 final class AdminDataviewAssetManifest
 {
     public const ASSET_KEY = 'ui.admin.dataview.css';
-    public const ASSET_REVISION = '20260819-3';
+    public const ASSET_REVISION = '20260819-4';
 
     /** @return array<string, mixed> */
     public static function publicationAsset(): array
